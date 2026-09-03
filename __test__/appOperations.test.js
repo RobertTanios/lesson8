1e0a01d441bceb291c9c7283bacc2cf313612b61
RobertTanios <rtanios@gmail.com>
2026-09-03 18:39:36 +0200

    fixed test cases and code coverage

--- __test__/appOperations.test.js
+++ __test__/appOperations.test.js
@@ -1,9 +1,9 @@
 const appOperations = require('../src/appOperations');
 
 test("multiplication property of zero", () => {
-  expect(appOperations(5, 0)).toBe(0);
+  expect(appOperations.multiply(5, 0)).toBe(0);
 });
 
-test('multiply 2 and 3 to equal 6', () => {
-  expect(appOperations(2, 3)).toBe(6);
+test('adding 2 values', () => {
+  expect(appOperations.add(2, 3)).toBe(5);
 });
