ea77784f19d1ce0c9be3230253fba52e23579c18
RobertTanios <rtanios@gmail.com>
2026-09-08 12:39:33 +0200

    added workflow to publish package and fix functinos

--- src/appOperations.js
+++ src/appOperations.js
@@ -1,17 +1,25 @@
 function multiply(num1, num2) {
-  return num1 * num2;
+  const intNum1 = parseInt(num1, 10);
+  const intNum2 = parseInt(num2, 10);
+  return intNum1 * intNum2;
 }
 
 function add(num1, num2) {
-  return num1 + num2;
+  const intNum1 = parseInt(num1, 10);
+  const intNum2 = parseInt(num2, 10);
+  return intNum1 + intNum2;
 }
 
 function substract(num1, num2) {
-  return num1 - num2;
+  const intNum1 = parseInt(num1, 10);
+  const intNum2 = parseInt(num2, 10);
+  return intNum1 - intNum2;
 }
 
 function divide(num1, num2) {
-  return num1 / num2;
+  const intNum1 = parseInt(num1, 10);
+  const intNum2 = parseInt(num2, 10);
+  return intNum1 / intNum2;
 }
 
 module.exports = { add, multiply, substract, divide };
